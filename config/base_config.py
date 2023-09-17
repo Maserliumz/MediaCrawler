6757b6860d1936408f635fb3e3f8b4f311122f2d
Maser <maserliumz@gmail.com>
2023-09-17 22:48:15 +0800

    增加贷款相关的关键词

--- config/base_config.py
+++ config/base_config.py
@@ -1,6 +1,6 @@
 # Desc: base config
 PLATFORM = "xhs"
-KEYWORDS = "python,golang"
+KEYWORDS = "分期,利率,借款,信用评分,贷款额度,贷款周期,利息,还款方式,无抵押,信贷,个人贷款,线上申请,额度审批,月供,年利率,逾期,信用卡贷款,贷款顾问,快速审批,资质审核,借贷平台,信用记录,借贷合同,首付,咨询服务,贷款计算器,违约金,征信,资金用途,金融机构,还款期限,贷款利息,抵押贷款,借贷风险,预审批,信用报告,申请条件,线下服务,贷款服务,贷款顾问,银行贷款,担保,信用卡分期,借贷利率,网贷,贷款申请流程,实时审批,灵活还款,贷款政策,提前还款"
 LOGIN_TYPE = "qrcode"  # qrcode or phone or cookie
 COOKIES = ""  # login by cookie, if login_type is cookie, you must set this value
 
